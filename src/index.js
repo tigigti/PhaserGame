c04685977f8ea1ffd4b2b1e501ba2272f7680f1f
Angelos Ioannou <angelos.ioannou@gmx.de>
2021-01-20 00:22:59 +0100

    init phaser game window

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log("hi");
+import Phaser from "phaser";
+
+function preload() {}
+
+function create() {
+    this.add.image(400, 300, "sky");
+}
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: { y: 200 },
+        },
+    },
+    scene: {
+        preload: preload,
+        create: create,
+    },
+};
+
+const game = new Phaser.Game(config);
